fce3073a5f976301559ab1b359b7b1a816b331ea
meryemctnky <meryemctnkya@gmail.com>
2022-09-19 12:30:35 +0300

    change image name

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
-import heroImg from "../../assets/images/hero-img1.png";
+import heroImg from "../../assets/images/hero-img1.jpg";
 
 
 const HeroSection = () => {
